bfa6cb6d50016a30b648bc3c22f5a3301971c116
HZH <2214600140@qq.com>
2018-09-07 09:47:21 +0800

    www

--- src/components/design.vue
+++ src/components/design.vue
@@ -11,33 +11,54 @@
         display: inline-block;
         position: absolute;
     }
+    .tools{
+        width: 100%;
+        height: 100%;
+        p{
+            height: 30px;
+            line-height: 30px;
+            padding-left: 10px;
+        }
+    }
 }
 </style>
 
 <template>
     <div id="design">
-        <VueDragResize 
-            :w="rect.width"
-            :h="rect.height"
-            :x="rect.left"
-            :y="rect.top"
+        <!-- <VueDragResize 
+            :w="designTool.width"
+            :h="designTool.height"
+            :x="designTool.left"
+            :y="designTool.top"
             :parentW="listWidth"
             :parentH="listHeight"
-            :axis="rect.axis"
-            :isActive="rect.active"
-            :minw="rect.minw"
-            :minh="rect.minh"
-            :isDraggable="rect.draggable"
-            :isResizable="rect.resizable"
-            :parentLimitation="rect.parentLim"
-            :aspectRatio="rect.aspectRatio"
-            :z="rect.zIndex"
-            v-on:activated="activateEv(index)"
-            v-on:deactivated="deactivateEv(index)"
-            v-on:dragging="changePosition($event, index)"
-            v-on:resizing="changeSize($event, index)"
+            :axis="designTool.axis"
+            :isActive="designTool.active"
+            :minw="designTool.minw"
+            :minh="designTool.minh"
+            :isDraggable="designTool.draggable"
+            :isResizable="designTool.resizable"
+            :parentLimitation="designTool.parentLim"
+            :aspectRatio="designTool.aspectRatio"
+            :z="designTool.zIndex"
+            v-on:activated="activateEv(-1)"
+            v-on:deactivated="deactivateEv(-1)"
+            v-on:dragging="changePosition($event, -1)"
+            v-on:resizing="changeSize($event, -1)"
         >
-        </VueDragResize>
+            <div class="tools">
+                <p>布局元素</p>
+                <div>
+                    <span>布局1</span>
+                </div>
+                <p>组件元素</p>
+                <div>
+                    <span>文字</span>
+                    <span>图片</span>
+                </div>
+            </div>
+        </VueDragResize> -->
+
         <VueDragResize v-for="(rect, index) in rects"
                     :key="index"
                     :w="rect.width"
@@ -60,20 +81,24 @@
                     v-on:dragging="changePosition($event, index)"
                     v-on:resizing="changeSize($event, index)"
         >
-            <div class="filler" :style="{backgroundColor:rect.color}">
+            <!-- <div class="filler" :style="{backgroundColor:rect.color}">
 
-            </div>
+            </div> -->
+            <myElement v-for="(element,index) of rect.allElements" :key="index" :style="element.style" :type="element.type" :config="element.config">
+            </myElement>
         </VueDragResize>
     </div>
 </template>
 
 <script>
 import VueDragResize from '@/components/design/dragResize.vue';
+import myElement from '@/components/display/myElement';
 
 export default {
     name: 'design',
     components: {
         VueDragResize,
+        myElement
     },
 
     data(){
@@ -97,6 +122,9 @@ export default {
     computed: {
         rects() {
             return this.$store.state.rect.rects
+        },
+        designTool(){
+            return this.$store.state.rect.designTool
         }
     },
 
--- src/store/modules/rect/state.js
+++ src/store/modules/rect/state.js
@@ -1,20 +1,90 @@
 export default {
-    'rects': [
-        {
-            'width': 200,
-            'height': 150,
-            'top': 10,
-            'left': 10,
-            'draggable': true,
-            'resizable': false,
-            'minw': 10,
-            'minh': 10,
-            'axis': 'both',
-            'parentLim': true,
-            'aspectRatio': false,
-            'zIndex': 1,
-            'color': '#EF9A9A',
-            'active': false
-        }
-    ]
+    'rects': [{
+        'width': 100,
+        'height': 30,
+        'top': 10,
+        'left': 10,
+        'draggable': true,
+        'resizable': false,
+        'minw': 100,
+        'minh': 100,
+        'axis': 'both',
+        'parentLim': true,
+        'aspectRatio': false,
+        'zIndex': 1,
+        'color': '#EF9A9A',
+        'active': false,
+        'allElements':[{
+            'type':'label',
+            'config':{
+                'text':'嘉兴市环境保护局',
+                'eleStyle':{
+                    'font-size':'32px',
+                    'color':'#305aa4',
+                    'font-weight': '900',
+                    'font-family': "宋体"
+                }
+            },
+            'style':{
+                'position':'absolute',
+                'top':'30px',
+                'left':'110px',
+                'margin-left':'0px',
+                'margin-top':'0px',
+                'z-index':'2'
+            }
+        }]
+    },
+    {
+        'width': 200,
+        'height': 200,
+        'top': 10,
+        'left': 10,
+        'draggable': true,
+        'resizable': false,
+        'minw': 100,
+        'minh': 100,
+        'axis': 'both',
+        'parentLim': true,
+        'aspectRatio': false,
+        'zIndex': 1,
+        'color': '#EF9A9A',
+        'active': false,
+        'allElements':[{
+            'type':'picture',
+            'config':{
+                'src':'images/test/Layer4.png',
+                'eleStyle':{
+
+                }
+            },
+            'style':{
+                'position':'absolute',
+                'top':'20px',
+                'left':'20px',
+                'margin-left':'0px',
+                'margin-top':'0px',
+                'width':'80px',
+                'height':'80px',
+                'z-index':'2'
+            }
+        }]
+    }
+    ],
+    'designTool':{
+        'width': 500,
+        'height': 500,
+        'top': 10,
+        'left': 10,
+        'draggable': true,
+        'resizable': false,
+        'minw': 100,
+        'minh': 100,
+        'axis': 'both',
+        'parentLim': true,
+        'aspectRatio': false,
+        'zIndex': 1,
+        'color': '#EF9A9A',
+        'active': false
+    }
 };
