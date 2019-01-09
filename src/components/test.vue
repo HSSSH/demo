e0b088a547727d15aee5c575d3dda7775860e5fb
HZH-PC\HZH <huzh@sucsoft.com>
2019-01-09 09:37:03 +0800

    sss

---
+++ src/components/test.vue
@@ -0,0 +1,33 @@
+<template>
+<div>
+    <!-- <table>
+        <tbody is="my-component"></tbody>
+    </table> -->
+    <div>
+        {{tree}}
+    </div>
+</div>
+</template>
+
+<script>
+import Vue from 'vue';
+Vue.component('my-component',{
+    template:'<div>XCXX</div>',
+    data(){
+        return{
+           
+        }
+    }
+})
+export default {
+    name:'test',
+    components:{},
+    data(){
+        return{
+            mess:'fsf',
+            tree:this.$store.state.rect.rects
+        }
+    }
+}
+</script>
+
